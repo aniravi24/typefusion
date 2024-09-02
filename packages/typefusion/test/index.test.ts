cc24dd75b2633b73b6779580a32ce0c8a264b885
Ani Ravi <5902976+aniravi24@users.noreply.github.com>
2024-09-02 02:53:52 -0400

    test(typefusion): add @effect/vitest

--- packages/typefusion/test/index.test.ts
+++ packages/typefusion/test/index.test.ts
@@ -1,13 +1,21 @@
 /* eslint-disable @typescript-eslint/require-await */
 import { describe, it } from "vitest";
 import typefusion from "../src/index.js";
+import typefusionEffect from "../src/effect.js";
+
+import { it as itE } from "@effect/vitest";
+
+const typefusionConfig = {
+  directory: "./test/examplejs/example",
+  ignoreGlob: ["**/src/**"],
+  verbose: false,
+  alwaysPrintExecutionGraph: true,
+};
 
 describe("typefusion", () => {
-  it("should run successfully", async () =>
-    typefusion({
-      directory: "./test/examplejs/example",
-      ignoreGlob: ["**/src/**"],
-      verbose: false,
-      alwaysPrintExecutionGraph: true,
-    }));
+  itE.effect("should run effect successfully", () =>
+    typefusionEffect(typefusionConfig),
+  );
+
+  it("should run successfully", async () => typefusion(typefusionConfig));
 });
