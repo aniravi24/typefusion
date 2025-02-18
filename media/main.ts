6ff4412a1450afe2200ebfc06aeb40b1693b928c
aniravi24 <5902976+aniravi24@users.noreply.github.com>
2025-02-18 06:08:47 +0000

    Deploying to gh-pages from @ aniravi24/typefusion@5c185618e51f8ce6fb03592385b03b789ace042f 🚀

--- media/main.ts
+++ media/main.ts
@@ -2,29 +2,29 @@ import { pgType } from "../src/db/postgres/types.js";
 import { TypefusionDbScript } from "../src/index.js";
 
 export const mainSchema = {
-  id: pgType.integer().notNull(),
-  name: pgType.text().notNull(),
+  address: pgType.text().notNull(),
   age: pgType.integer().notNull(),
   email: pgType.text().notNull(),
-  address: pgType.text().notNull(),
+  id: pgType.integer().notNull(),
+  name: pgType.text().notNull(),
 };
 
 export default {
   name: "main",
-  schema: mainSchema,
   resultDatabase: "postgresql",
   run: async () => {
     console.log("MAIN IS RUN");
     return {
       data: [
         {
-          id: 1,
-          name: "John Doe",
+          address: "123 Main St",
           age: 30,
           email: "john.doe@example.com",
-          address: "123 Main St",
+          id: 1,
+          name: "John Doe",
         },
       ],
     };
   },
+  schema: mainSchema,
 } satisfies TypefusionDbScript<typeof mainSchema>;
